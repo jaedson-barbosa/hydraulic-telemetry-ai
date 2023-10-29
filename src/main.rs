a6e1ada6239c0949fd894185b0b125338b2cb2cc
Jaedson <jaedson33@gmail.com>
2023-10-29 19:48:08 -0300

    final commit for presentation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use neuroflow::FeedForward;
-use plotters::prelude::{ChartBuilder, IntoDrawingArea, PathElement};
+use plotters::prelude::{ChartBuilder, IntoDrawingArea, PathElement, SeriesLabelPosition};
 use plotters::series::LineSeries;
-use plotters::style::{RED, WHITE, BLACK, BLUE};
+use plotters::style::{BLACK, BLUE, RED, WHITE};
 use rand::distributions::Uniform;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
@@ -59,7 +59,7 @@ fn load_data(path: &str) -> (Vec<DeviceState>, Vec<[f32; 4]>) {
             let percentage = (max - item[1].time_sec as f32) / max;
             item[1].get_ai_data(item[0], percentage)
         })
-        .collect::<Vec<[f32; 4]>>();
+        .collect();
     (data, input_data)
 }
 
@@ -69,15 +69,17 @@ fn main() {
     let mut rng = thread_rng();
     data.shuffle(&mut rng);
 
-    let split_pos = data.len() * 6 / 10;
-    let (train_data, _) = data.split_at_mut(split_pos);
-
-    let mut nn = FeedForward::new(&[3, 7, 3, 1], || rand::random::<f32>());
-    for _ in 0..10_000_000 {
-        let mut rnd_range = thread_rng();
-        let i = rnd_range.sample(Uniform::new(0, train_data.len()));
-        nn.fit(&train_data[i][..3], &train_data[i][3..])
+    let train_len = data.len() / 20;
+    let start = std::time::Instant::now();
+    let mut nn = FeedForward::new(&[3, 3, 3, 1], || rand::random::<f32>());
+    nn.learn_rate = 0.5;
+    for i in 0..100_000 {
+        let item = data[i % train_len];
+        nn.fit(&item[..3], &[item[3]])
     }
+    let end = std::time::Instant::now();
+    let elapsed = (end - start).as_millis();
+    println!("Training time: {elapsed} ms");
 
     let mut writer = csv::Writer::from_path("out/result.csv").unwrap();
     let mut total_abs_error = 0.0;
@@ -85,12 +87,17 @@ fn main() {
     let mut x = Vec::new();
     let mut y = Vec::new();
     let mut y_nn = Vec::new();
-    data.sort_by(|a,b| if a[3] < b[3] { Ordering::Greater } else { Ordering::Less });
+    data.sort_by(|a, b| {
+        if a[3] < b[3] {
+            Ordering::Greater
+        } else {
+            Ordering::Less
+        }
+    });
     for (i, item) in data.iter().enumerate() {
         let input = &item[..3];
         let output = nn.calc(input)[0];
         let expected = item[3];
-        println!("for {input:?}, {expected} -> {output}");
 
         let difference = expected - output;
         total_abs_error += difference.abs();
@@ -104,7 +111,7 @@ fn main() {
         writer.serialize(test).unwrap();
         writer.flush().unwrap();
 
-        x.push(source_data[i].time_sec);
+        x.push((source_data[i].time_sec as f32) / 3600.0);
         y.push(expected);
         y_nn.push(output);
     }
@@ -117,31 +124,60 @@ fn main() {
     let str = postcard::to_vec::<_, 1024>(&nn).unwrap();
     std::fs::write("model/model", &str).unwrap();
 
-    let root = plotters_bitmap::BitMapBackend::new("0.png", (720, 480)).into_drawing_area();
+    let root = plotters_svg::SVGBackend::new("soc1.svg", (550, 360)).into_drawing_area();
     root.fill(&WHITE).unwrap();
-    let x_end = source_data.last().unwrap().time_sec as f32;
+    let x_end = (source_data.last().unwrap().time_sec as f32) / 3600.0;
+    let min_time = (source_data[0].time_sec as f32) / 3600.0;
+    let min_bat_v = (source_data.last().unwrap().adc_state.battery_mv as f32) / 1000.0;
+    let max_bat_v = (source_data[0].adc_state.battery_mv as f32) / 1000.0;
     let mut chart = ChartBuilder::on(&root)
         .margin(5)
-        .set_left_and_bottom_label_area_size(30)
-        .build_cartesian_2d(0.0..x_end, 0.0..1.0f32)
+        .set_left_and_bottom_label_area_size(40)
+        .right_y_label_area_size(40)
+        .build_cartesian_2d(min_time..x_end, 0.0..100.0f32)
+        .unwrap()
+        .set_secondary_coord(min_time..x_end, min_bat_v..max_bat_v);
+    chart
+        .configure_mesh()
+        .x_desc("Tempo (h)")
+        .y_desc("SOC (%)")
+        .draw()
         .unwrap();
-    chart.configure_mesh().draw().unwrap();
-    let values = x.iter().zip(y.iter()).map(|v| (*v.0 as f32, *v.1));
+    chart
+        .configure_secondary_axes()
+        .y_desc("Tensão (V)")
+        .draw()
+        .unwrap();
+    let values = x.iter().zip(y.iter()).map(|v| (*v.0, *v.1 * 100.0));
     chart
         .draw_series(LineSeries::new(values, &RED))
         .unwrap()
-        .label("correct soc")
+        .label("SOC correto")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-    let values = x.iter().zip(y_nn.iter()).map(|v| (*v.0 as f32, *v.1));
+    let values = x
+        .iter()
+        .zip(y_nn.iter())
+        .map(|v| (*v.0, *v.1 * 100.0));
     chart
         .draw_series(LineSeries::new(values, &BLUE))
         .unwrap()
-        .label("estimated soc")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+        .label("SOC estimado")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+    let values = x
+        .iter()
+        .zip(source_data.iter().map(|v| v.adc_state.battery_mv as f32))
+        .map(|v| (*v.0, v.1 / 1000.0));
+    chart
+        .draw_secondary_series(LineSeries::new(values, &BLACK))
+        .unwrap()
+        .label("Tensão bat.")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));
     chart
         .configure_series_labels()
+        .position(SeriesLabelPosition::UpperRight)
         .background_style(&WHITE)
         .border_style(&BLACK)
-        .draw().unwrap();
+        .draw()
+        .unwrap();
     root.present().unwrap();
 }
